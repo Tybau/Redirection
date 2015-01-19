321240f58566687f393f1df459c7fcc24548324a
Tybau <gomusic.sisium@gmail.com>
2015-01-19 16:40:18 +0100

    Niveaux
    Modification des niveaux et refonte du code.

--- src/fr/thibault/redirection/Jeu.java
+++ src/fr/thibault/redirection/Jeu.java
@@ -10,13 +10,12 @@ import fr.thibault.redirection.niveau.Niveau;
 public class Jeu {
 	
 	Input input;
-	Graphics g;
 	
 	Niveau niv;
 	
 	public void init(GameContainer container) throws SlickException{
 		input = container.getInput();
-		niv = new Niveau(1, 6);
+		niv = new Niveau(0, 6);
 	}
 	
 	public void update(GameContainer container, int delta){
